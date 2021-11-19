ccc32f6005ddd8f97858e9e3d56ba7db637511a6
shacharTzur <shachartzur95@gmail.com>
2021-11-19 15:53:45 +0200

    Commit test

--- src/main/java/com/mymarketplace/controller/HomeController.java
+++ src/main/java/com/mymarketplace/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping("/asdddddddd")
     public String homepage(){
         return "homepage";
     }
