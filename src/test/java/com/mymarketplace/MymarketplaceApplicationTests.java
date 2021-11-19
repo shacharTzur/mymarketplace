e7d077b7eceefea557ca63bfe1b8f034611ef3fd
shacharTzur <shachartzur95@gmail.com>
2021-11-19 15:37:49 +0200

    adding files

---
+++ src/test/java/com/mymarketplace/MymarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
