89cc3c8020231b8abcb613c723ae399c4b4aacee
shacharTzur <shachartzur95@gmail.com>
2021-12-06 22:23:27 +0200

    Upload

--- src/main/java/com/mymarketplace/controller/HomeController.java
+++ src/main/java/com/mymarketplace/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.mymarketplace.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.Response;
@@ -12,7 +10,7 @@ import javax.xml.ws.Response;
 public class HomeController {
     @RequestMapping( "/")
     public String homepage(){
-        return "homepage";
+        return "index";
     }
 
 }
