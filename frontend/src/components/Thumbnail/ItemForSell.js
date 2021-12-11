9531ae67d06e610339f91f2db349e950ae981f67
Ido <idoh94@hotmail.com>
2021-12-11 19:33:52 +0200

    added changes to ItemForSell.js to test communication with server

--- frontend/src/components/Thumbnail/ItemForSell.js
+++ frontend/src/components/Thumbnail/ItemForSell.js
@@ -4,6 +4,9 @@ import ProfileBubble from "./ProfileBubble";
 function ItemForSell(props) {
     function profileButtonHandler(){
         console.log('Clicked')
+        fetch('http://localhost:8080/user/all')
+          .then(response => response.json())
+          .then(data => console.log(data))
 
     }
     return (
@@ -11,7 +14,9 @@ function ItemForSell(props) {
             <h3>{props.name}</h3>
             <ProfileBubble name='profile name' />
             <button>className={classes.btn} onClick={profileButtonHandler}</button>
+            <button onClick={profileButtonHandler}>Hi</button>
         </div>
+            
     );
 };
 
