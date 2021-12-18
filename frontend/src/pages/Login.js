85e7917d286601e04b57b1f824bf75427f1be3c6
Ido <idoh94@hotmail.com>
2021-12-18 13:11:57 +0200

    new front

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,115 @@
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import {Container as ContainerBase} from "components/misc/Layouts";
+import tw from "twin.macro";
+import styled from "styled-components";
+import {css} from "styled-components/macro"; //eslint-disable-line
+// import illustration from "images/login-illustration.svg";
+import illustration from "images/ourImages/shopping bags logo.jpg";
+import logo from "images/ourImages/pink cart logo.jpg";
+import googleIconImageSrc from "images/google-icon.png";
+import twitterIconImageSrc from "images/twitter-icon.png";
+import {ReactComponent as LoginIcon} from "feather-icons/dist/icons/log-in.svg";
+import { useState, useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+// import AuthContext from '../../store/auth-context';
+
+const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
+const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
+const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
+const LogoLink = tw.a``;
+const LogoImage = tw.img`h-20 mx-auto rounded-full`;
+const MainContent = tw.div`mt-12 flex flex-col items-center`;
+const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
+const FormContainer = tw.div`w-full flex-1 mt-8`;
+
+
+const DividerTextContainer = tw.div`my-12 border-b text-center relative`;
+const DividerText = tw.div`leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform -translate-y-1/2 absolute inset-x-0 top-1/2 bg-transparent`;
+
+const Form = tw.form`mx-auto max-w-xs`;
+const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+const SubmitButton = styled.button  ${tw`mt-5 tracking-wide font-semibold bg-primary-`
+500 text-gray-100 w-full py-4 rounded-lg hover:bg-primary-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
+  .icon {
+    ${tw`w-6 h-6 -ml-2`}
+  }
+
+  .text {
+    ${tw`ml-3`}
+  }
+`;
+const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
+const IllustrationImage = styled.div`
+  ${props => `background-image: url("${props.imageSrc}");`}
+  ${tw`m-12 xl:m-16 w-full max-w-sm bg-contain bg-center bg-no-repeat`}
+`;
+export default ({
+                    logoLinkUrl = "#",
+                    illustrationImageSrc = illustration,
+                    headingText = "Sign In To MyMarketPlace",
+                    submitButtonText = "Sign In",
+                    SubmitButtonIcon = LoginIcon,
+                    forgotPasswordUrl = "#",
+                    signupUrl = "#",
+
+                }) => {
+    const history = useHistory();
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        // add validation
+        let url;
+        url = 'http://localhost:8080/user/name?userName='+enteredEmail;
+        fetch(url).then(res => res.json()
+        ).then(data => {
+            console.log(data);
+            history.push('/components/innerPages/BlogIndexPage');
+        });
+        // history.push('/components/innerPages/BlogIndexPage');
+    };
+    return (
+        <AnimationRevealPage>
+            <Container>
+                <Content>
+                    <MainContainer>
+                        <LogoLink href={logoLinkUrl}>
+                            <LogoImage src={logo}/>
+                        </LogoLink>
+                        <MainContent>
+                            <Heading>{headingText}</Heading>
+                            <FormContainer>
+                                <Form onSubmit={submitHandler}>
+                                    <Input type="text" placeholder="Email" ref={emailInputRef}/>
+                                    <Input type="password" placeholder="Password" ref={passwordInputRef}/>
+                                    <SubmitButton type="submit">
+                                        <SubmitButtonIcon className="icon"/>
+                                        <span className="text">{submitButtonText}</span>
+                                    </SubmitButton>
+                                </Form>
+                                <p tw="mt-6 text-xs text-gray-600 text-center">
+                                    <a href={forgotPasswordUrl} tw="border-b border-gray-500 border-dotted">
+                                        Forgot Password ?
+                                    </a>
+                                </p>
+                                <p tw="mt-8 text-sm text-gray-600 text-center">
+                                    Dont have an account?{" "}
+                                    <a href={signupUrl} tw="border-b border-gray-500 border-dotted">
+                                        Sign Up
+                                    </a>
+                                </p>
+                            </FormContainer>
+                        </MainContent>
+                    </MainContainer>
+                    <IllustrationContainer>
+                        <IllustrationImage imageSrc={illustrationImageSrc}/>
+                    </IllustrationContainer>
+                </Content>
+            </Container>
+        </AnimationRevealPage>
+    );
+};
