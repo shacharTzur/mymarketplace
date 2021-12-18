78dca0af1489619be119cb3d10a868ea36d2cc5d
Ido <idoh94@hotmail.com>
2021-12-18 18:34:34 +0200

    login context fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Modal from "react-modal";
+import { AuthContextProvider } from './store/auth-context';
 
 Modal.setAppElement("#root");
 
 ReactDOM.render(
+  <AuthContextProvider>
   <App />,
+  </AuthContextProvider>,
   document.getElementById("root")
 );
--- frontend/src/store/auth-context.js
+++ frontend/src/store/auth-context.js
@@ -1,43 +1,37 @@
 import React, {useState, useEffect} from 'react';
 
-let logoutTimer;
-
 const AuthContext = React.createContext({
     token: '',
     isLoggedIn: false,
-    login: (token) => {
-        alert(token)
-    },
-    logout: () => {
-    }
+    login: (token) => {},
+    logout: () => {}
 });
 
 // const retrieveStoredToken = () => {
 //     const storedToken = localStorage.getItem('token');
-//     return {
-//         token: storedToken
-//     }
+//     return {token: storedToken};
+// };
 
 export const AuthContextProvider = (props) => {
-    const tokenData = localStorage.getItem('token');
-    let initialToken;
-    if (tokenData){
-        initialToken = tokenData.token
-    }
+    // const tokenData = localStorage.getItem('token');
+    // let initialToken;
+    // if (tokenData){
+    //     initialToken = tokenData.token
+    // }
+    const initialToken = localStorage.getItem('token');
     const [token, setToken] = useState(initialToken);
     const userisLoggedIn = !!token;
+
+    // const loginHandler = (token, expirationTime) => {
+
+    const loginHandler = (token) => {
+        setToken(token);
+        localStorage.setItem('token', token);
+    };
 
     const logoutHandler = () => {
         setToken(null);
         localStorage.removeItem('token');
-    };
-
-    // const loginHandler = (token, expirationTime) => {
-    const loginHandler = (token) => {
-        setToken(token);
-        // alert(token)
-        localStorage.setItem('tokenn', token);
-
     };
 
     const contextValue = {
