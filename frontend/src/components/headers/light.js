34d5a72e579af5af314ed50156af6f24bfabf362
noa <noabennathan@gmail.com>
2021-12-31 15:15:53 +0200

    lights.add

--- frontend/src/components/headers/light.js
+++ frontend/src/components/headers/light.js
@@ -80,7 +80,6 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
   }
   const defaultLinks = [
     <NavLinks key={1}>
-
       <NavLink href="/#">Marketplace</NavLink>
       <NavLink href="/components/innerPages/AddItemPage">Sell Item</NavLink>
       <NavLink href="/components/innerPages/IWantPage">I Want</NavLink>
@@ -89,7 +88,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
       {/*  Login*/}
       {/*</NavLink>*/}
       {isLoggedIn && (
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#" onClick={logoutHandler}>Sign Out</PrimaryLink>
+          <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#" onClick={logoutHandler}>Sign Out</PrimaryLink>
       )}
     </NavLinks>
   ];
@@ -98,32 +97,32 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
   const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
   const defaultLogoLink = (
-    <LogoLink href="/">
-      <img src={logo} alt="logo" />
-      MyMarketPlace
-    </LogoLink>
+      <LogoLink href="/">
+        <img src={logo} alt="logo" />
+        MyMarketPlace
+      </LogoLink>
   );
 
   logoLink = logoLink || defaultLogoLink;
   links = links || defaultLinks;
 
   return (
-    <Header className={className || "header-light"}>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
-        {logoLink}
-        {links}
-      </DesktopNavLinks>
-
-      <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
-        {logoLink}
-        <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
+      <Header className={className || "header-light"}>
+        <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
+          {logoLink}
           {links}
-        </MobileNavLinks>
-        <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
-          {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
-        </NavToggle>
-      </MobileNavLinksContainer>
-    </Header>
+        </DesktopNavLinks>
+
+        <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
+          {logoLink}
+          <MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>
+            {links}
+          </MobileNavLinks>
+          <NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>
+            {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}
+          </NavToggle>
+        </MobileNavLinksContainer>
+      </Header>
   );
 };
 
