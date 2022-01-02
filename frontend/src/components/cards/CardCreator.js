461d8b9c5243c8b54fcf3398accec7bcdc060a83
Ido <idoh94@hotmail.com>
2022-01-02 13:54:26 +0200

    removed password field and fixed homepage products pull

--- frontend/src/components/cards/CardCreator.js
+++ frontend/src/components/cards/CardCreator.js
@@ -1,9 +1,13 @@
 import React, {useEffect, useState} from "react";
+import { useContext } from 'react'
+import AuthContext from '../../store/auth-context';
 
 function Listing() {
     const [data, setData] = useState([]);
+    const authCtx = useContext(AuthContext);
+    let url = 'http://localhost:8080/product/allForYou?username=' + authCtx.token;
     useEffect(() => {
-        fetch('http://localhost:8080/product/all')
+        fetch(url)
             .then(function (response) {
                 console.log(response)
                 return response.json();
