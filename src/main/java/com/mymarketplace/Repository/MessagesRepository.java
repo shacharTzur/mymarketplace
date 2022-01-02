030734f56d5b1680e536e421ddbdd7926e24f01a
shacharTzur <shachartzur95@gmail.com>
2022-01-02 18:49:33 +0200

    Commit for change in messages controller to retrieve all messages for chat between 2 users

--- src/main/java/com/mymarketplace/Repository/MessagesRepository.java
+++ src/main/java/com/mymarketplace/Repository/MessagesRepository.java
@@ -2,18 +2,27 @@ package com.mymarketplace.Repository;
 
 
 import com.mymarketplace.Entities.MessagesEntity;
+import com.mymarketplace.Entities.ProductEntity;
 import com.mymarketplace.Entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Repository
 public interface MessagesRepository extends JpaRepository<MessagesEntity, Long> {
 
-    List<MessagesEntity> findByFrom(String From);
+    List<MessagesEntity> findBySender(String Sender);
 
-    List<MessagesEntity> findByTo(String To);
+    List<MessagesEntity> findByReceiver(String Receiver);
 
+    @Query(value = "Select * From MESSAGES WHERE (sender Like :sender and receiver Like :receiver) or (sender Like :receiver and receiver Like :sender) and product_id Like :product_id" , nativeQuery = true)
+    List<MessagesEntity> findChatForUsers
+            (@RequestParam ("sender") String sender,
+             @RequestParam ("receiver") String receiver,
+             @RequestParam ("product_id") Long product_id);
 
 }
